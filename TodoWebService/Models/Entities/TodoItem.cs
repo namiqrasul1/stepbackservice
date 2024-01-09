85e25a093c38a304bf872dc4321f39198167af8b
namiqrasul1 <namiq_rasullu@itstep.org>
2024-01-09 17:37:57 +0400

    init

---
+++ TodoWebService/Models/Entities/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoWebService.Models.Entities
+{
+    public class TodoItem
+    {
+        // 0515c4df-db95-44a5-ab88-387600970e72
+        // 0515c4df-db95-44a5-ab88-387600970e72
+        // 0515c4df-db95-44a5-ab88-387600970e72
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public virtual AppUser User { get; set; }
+
+    }
+}
---
+++ TodoWebService/Program.cs
@@ -0,0 +1,61 @@
+using FluentValidation;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using TodoWebService;
+using TodoWebService.Data;
+using TodoWebService.Models.DTOs.Validations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAnyOrigin", builder =>
+//    {
+//        builder.AllowAnyOrigin()
+//               .AllowAnyHeader()
+//               .AllowAnyMethod();
+//    });
+//});
+
+
+builder.Services.AddDomainServices();
+
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoDbConnectionString"));
+});
+
+builder.Services.AddAuthenticationAndAuthorization(builder.Configuration);
+
+builder.Services.AddSwagger();
+
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseCors("AllowAnyOrigin");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
